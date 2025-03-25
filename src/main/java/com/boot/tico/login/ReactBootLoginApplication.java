8383b238804b354bbaf64b1432355272fb08cb22
kilminjong <qkqk0110@naver.com>
2025-03-25 23:27:41 +0900

    [feat]ReactBootLoginApplication.java
    -Spring Boot 애플리케이션 시작: @SpringBootApplication 어노테이션을 통해 Spring Boot 애플리케이션임을 선언하고, 필요한 설정을 자동으로 구성 가능
    
    - 메인 메서드 실행: main 메서드를 통해 애플리케이션을 실행하며, SpringApplication.run() 메서드를 호출하여 Spring Boot 애플리케이션을 시작

---
+++ src/main/java/com/boot/tico/login/ReactBootLoginApplication.java
@@ -0,0 +1,13 @@
+package com.boot.tico.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBootLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBootLoginApplication.class, args);
+	}
+
+}
