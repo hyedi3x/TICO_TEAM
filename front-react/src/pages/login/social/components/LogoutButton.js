ced66c836176d689de03d16eadf25ce4e813930d
kilminjong <qkqk0110@naver.com>
2025-03-27 14:36:00 +0900

    [modify]LogoutButton.js
    - 네이버 로그아웃 버튼 추가구현

--- front-react/src/pages/login/social/components/LogoutButton.js
+++ front-react/src/pages/login/social/components/LogoutButton.js
@@ -1,25 +1,30 @@
 // components/LogoutButton.js
 import React from 'react';
+import { logoutFromKakao, logoutFromNaver } from "../services/authService";
 
 const LogoutButton = ({ onLogout }) => {
+  
+  const handleLogout = async () => {
+    await logoutFromKakao(); // 카카오 로그아웃 실행
+    await logoutFromNaver(); // 네이버 로그아웃 실행
+    onLogout(); // 상태 초기화
+  };
 
-    return (
-        <button 
-            onClick={onLogout}
-            style={{
-                backgroundColor: '#FEE500',
-                border: 'none',
-                padding: '10px 20px',
-                cursor: 'pointer',
-                borderRadius: '5px',
-                fontWeight: 'bold',
-            }}
-        >
-            카카오 로그아웃
-        </button>
-
-        
-    );
+  return (
+    <button 
+      onClick={handleLogout}
+      style={{
+        backgroundColor: '#FEE500',
+        border: 'none',
+        padding: '10px 20px',
+        cursor: 'pointer',
+        borderRadius: '5px',
+        fontWeight: 'bold',
+      }}
+    >
+      로그아웃
+    </button>
+  );
 };
 
 export default LogoutButton;
