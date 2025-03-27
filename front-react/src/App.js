1f96eeb0800eb1c242dc2282e4df42d503e4e47e
cruelSummer12 <rlatkdgus100@naver.com>
2025-03-27 10:25:40 +0900

    [feat] App.js 작품만들기 경로 추가

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -3,16 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './common/Header';
 import Main from './common/Main';
 import Footer from './common/Footer';
-
+import BlocklyComponent from './blockly/components/BlocklyComponent'
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path="/" exact={true} element={<Main/>}/> 
+          <Route path="/" exact={true} element={<Main />} />
+          <Route path="/createBlock" exact={true} element={<BlocklyComponent />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
