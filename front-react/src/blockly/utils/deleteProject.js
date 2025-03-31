331229316a9c53081570a2d37d64937ed1d89cdf
wpzmfhtm <wpzmfhtm@naver.com>
2025-03-31 17:46:00 +0900

    [feat] saveProject.js & loadProjects.js & loadProjectDetail.js & deleteProject.js
    Blockly 작품 CRUD 관련 리액트 코드(전부 axios, async, await로 비동기 처리)
    - saveProject.js => PK값이 null값으로 없으면 INSERT, 있으면 UPDATE 처리
    - loadProjects.js => 전체 작품 목록 SELECT
    - loadProjectDetail.js => 전체 작품 목록에서 1개의 작품의 정보만 SELECT
    - deleteProject.js => 작품 DELETE  처리

---
+++ front-react/src/blockly/utils/deleteProject.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const handleDeleteProject = async (projectId) => {
+    if(!projectId){
+        alert("삭제할 프로젝트가 존재하지 않습니다");
+        return;
+    }
+
+    const confirmed = window.confirm("정말로 이 프로젝트를 삭제하시겠습니까?");
+    if(!confirmed) return;
+
+    try{
+        await axios.delete(`http://localhost:8081/project/deleteProject/${projectId}`);
+        alert("삭제 완료!");
+        window.location.reload(); // 새로고침
+    }catch(err){
+        console.error("삭제 실패", err);
+        alert("삭제 중 문제가 발생했습니다.");
+    }
+}
