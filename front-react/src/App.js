00298334b29e67a09172e9247a8bc166deed4573
wpzmfhtm <wpzmfhtm@naver.com>
2025-04-01 15:54:20 +0900

    [fix] FAQ 관련 컴포넌트 및 임포트 제거

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -4,9 +4,6 @@ import Header from './common/Header';
 import Main from './common/Main';
 import Footer from './common/Footer';
 import BlocklyComponent from './blockly/components/BlocklyComponent'
-import FAQList from './blockly/blocks/FAQ/FAQList';
-import FAQPost from './blockly/blocks/FAQ/FAQPost';
-import FAQPut from './blockly/blocks/FAQ/FAQPut';
 function App() {
   return (
     <div>
@@ -15,9 +12,6 @@ function App() {
         <Routes>
           <Route path="/" exact={true} element={<Main />} />
           <Route path="/createBlock" exact={true} element={<BlocklyComponent />} />
-          <Route path="/FAQList" exact={true} element={<FAQList />} />
-          <Route path="/FaqPost" exact={true} element={<FAQPost />} />
-          <Route path="/FAQPut/:qa_id" exact={true} element={<FAQPut />} />
         </Routes>
         <Footer />
       </BrowserRouter>
