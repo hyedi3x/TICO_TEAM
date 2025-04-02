56baf35ebafaee34ca1128654cdcec7ad1c82384
wpzmfhtm <wpzmfhtm@naver.com>
2025-04-02 10:39:53 +0900

    [modify] @crossorigin 제거(전역 CORS로 방식 변경)

--- src/main/java/com/boot/tico/project/controller/ProjectController.java
+++ src/main/java/com/boot/tico/project/controller/ProjectController.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/project")
 public class ProjectController {
