89a55313930d5332a30aed281f617f77359ae52b
kilminjong <qkqk0110@naver.com>
2025-04-09 16:33:02 +0900

    [Modify] Springboot code 수정
    - 비동기화 설정을위해 @EnableAsync 어노테이션 추가

--- src/main/java/com/boot/tico/TicoTeamApplication.java
+++ src/main/java/com/boot/tico/TicoTeamApplication.java
@@ -2,8 +2,10 @@ package com.boot.tico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync  // 비동기 처리를 활성화합니다.
 public class TicoTeamApplication {
 
 	public static void main(String[] args) {
