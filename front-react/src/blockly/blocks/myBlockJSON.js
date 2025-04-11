49ff5400b6e62204f81ec7fdd23f36895f2ba49d
wpzmfhtm <wpzmfhtm@naver.com>
2025-04-11 18:30:16 +0900

    [fix] 커스텀 블록 추가 및 설정 변경
    - 직접 색을 추가하지 않고 테마의 style명을 적용해서 그 색을 블록과 카테고리에 적용하는 방식으로 변경(colour -> style)
    - 시작 카테고리: 마우스 클릭
    - 흐름 카테고리: 복제본 생성, 복제본만 컨트를, 복제본 삭제
    - 움직임 카테고리: 이동방향으로 이동, 이동방향 설정, X와Y 좌표를 변경, 마우스 위치로 이동
    - 소리 카테고리: 소리 크기 줄이기(단위 : %)
    - 판단 카테고리: 접촉 여부

--- front-react/src/blockly/blocks/myBlockJSON.js
+++ front-react/src/blockly/blocks/myBlockJSON.js
@@ -1,4 +1,5 @@
 import * as Blockly from 'blockly';
+import { getCloneDropdown, getTouchDropdown} from './dropdown';
 
 const defineMyBlocks = ()=>{
 
@@ -9,9 +10,11 @@ const defineMyBlocks = ()=>{
       {
         "type": "start_btn",
         "message0": "시작하기",
-        "colour": "#FF6666",
+        "style": "start_blocks",
         "nextStatement": null
       },
+
+      // 선택한 키 눌렀을 때의 블록
       {
         "type": "start_with_q",
         "message0": "%1 버튼을 눌러 시작하기",
@@ -36,11 +39,65 @@ const defineMyBlocks = ()=>{
           ]
         }
       ],
-        "colour": "#FF6666",
+        "style": "start_blocks",
         "nextStatement": null
       },
 
+      // 마우스 눌렀을 때의 블록
+      {
+        "type": "start_mouse_clicked",
+        "message0": "마우스를 클릭했을 때",
+        "nextStatement": null,
+        "style": "start_blocks",
+        "tooltip": "마우스를 클릭하면 연결된 블록들을 실행합니다.",
+        "helpUrl": ""
+      },
+      
       // 흐름 관련 블록들
+      // 복제본 생성하기 블록
+      {
+        "type": "create_clone",
+        "message0": "%1 의 복제본 생성하기",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "TARGET",
+            "options": getCloneDropdown
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "style": "loop_blocks",
+        "tooltip": "선택한 오브젝트의 복제본을 생성합니다.",
+        "helpUrl": ""
+      },     
+
+      // 생성된 복제본을 컨트롤하는 블록
+      {
+        "type": "on_clone_created",
+        "message0": "복제본이 생성되었을 때 %1",
+        "args0": [
+          {
+            "type": "input_statement",
+            "name": "DO"
+          }
+        ],
+        "style": "loop_blocks",
+        "tooltip": "복제본이 생성될 때 실행할 블록들을 연결합니다.",
+        "helpUrl": ""
+      },      
+
+      // 복제본 삭제하기 블록
+      {
+        "type": "delete_this_clone",
+        "message0": "이 복제본 삭제하기",
+        "previousStatement": null,
+        "nextStatement": null,
+        "style": "loop_blocks",
+        "tooltip": "현재 실행 중인 복제본을 삭제합니다.",
+        "helpUrl": ""
+      },      
+
       // 조건이 참이 될 때까지 반복하는 블록
       {
         "type": "wait_until_true",
@@ -54,7 +111,7 @@ const defineMyBlocks = ()=>{
         ],
         "previousStatement": null,
         "nextStatement": null,
-        "colour": "#FF9966",
+        "style": "loop_blocks",
         "tooltip": "조건이 참이 될 때까지 기다립니다.",
         "helpUrl": ""
       },
@@ -73,15 +130,53 @@ const defineMyBlocks = ()=>{
         ],
         "previousStatement": null,
         "nextStatement": null,
-        "colour": "#FF9966",
+        "style": "loop_blocks",
         "tooltip": "설정한 시간(초)만큼 기다립니다.",
         "helpUrl": ""
       },      
 
       // 움직임 관련 블록들
-      // 이동 방향과 거리 블록
+      // 이동방향으로 입력값만큼 이동하는 블록
       {
         "type": "move_in_direction",
+        "message0": "이동 방향으로 %1만큼 이동하기",
+        "args0": [
+          {
+            "type": "field_input",
+            "name": "distance",
+            "check": "Number",
+            "align": "RIGHT"
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "style": "move_blocks",
+        "tooltip": "이동방향과 일치하는 각도로 입력한 거리만큼 요소를 이동시킵니다.",
+        "helpUrl": ""
+      },
+
+      // 이동방향으로 입력값만큼 이동하는 블록
+      {
+        "type": "change_move_direction",
+        "message0": "이동 방향을 %1°로 정하기",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "direction",
+            "check": "Number",
+            "align": "RIGHT"
+          }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "style": "move_blocks",
+        "tooltip": "이동방향과 일치하는 각도로 입력한 거리만큼 요소를 이동시킵니다.",
+        "helpUrl": ""
+      },
+
+      // 입력한 각도와 거리로 이동하는 블록
+      {
+        "type": "move_in_direction_angle",
         "message0": "%1° 방향으로 %2만큼 이동하기",
         "args0": [
           {
@@ -99,7 +194,7 @@ const defineMyBlocks = ()=>{
         ],
         "previousStatement": null,
         "nextStatement": null,
-        "colour": "#FFCC66",
+        "style": "move_blocks",
         "tooltip": "입력한 방향의 각도와 거리만큼 요소를 이동시킵니다.",
         "helpUrl": ""
       },
@@ -117,7 +212,7 @@ const defineMyBlocks = ()=>{
         ],
         "previousStatement": null,
         "nextStatement": null,
-        "colour": "#FFCC66",
+        "style": "move_blocks",
         "tooltip": "x 좌표를 지정된 값만큼 움직입니다.",
         "helpUrl": ""
      },
@@ -128,14 +223,14 @@ const defineMyBlocks = ()=>{
       "message0": "y 좌표로 %1 만큼 움직이기",
       "args0": [
           {
-              "type": "field_input",
-              "name": "y",
-              "text": "10", // 기본값
+            "type": "field_input",
+            "name": "y",
+            "text": "10", // 기본값
           }
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#FFCC66",
+      "style": "move_blocks",
       "tooltip": "y 좌표를 지정된 값만큼 움직입니다.",
       "helpUrl": ""
     },
@@ -145,9 +240,9 @@ const defineMyBlocks = ()=>{
       "message0": "x 좌표로 %1 만큼, y 좌표로 %2만큼 움직이기",
       "args0": [
           {
-              "type": "field_input",
-              "name": "x",
-              "text": "10", // 기본값
+            "type": "field_input",
+            "name": "x",
+            "text": "10", // 기본값
           },
           {
             "type": "field_input",
@@ -157,7 +252,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#FFCC66",
+      "style": "move_blocks",
       "tooltip": "x,y 좌표를 지정된 값만큼 움직입니다.",
       "helpUrl": ""
     },
@@ -176,7 +271,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#FFCC66",
+      "style": "move_blocks",
       "tooltip": "입력한 x좌표로 요소를 이동시킵니다.",
       "helpUrl": ""
     },
@@ -195,8 +290,33 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#FFCC66",
+      "style": "move_blocks",
       "tooltip": "입력한 y좌표로 요소를 이동시킵니다.",
+      "helpUrl": ""
+    },
+
+    // 해당 X,Y좌표로 이동하기 블록
+    {
+      "type": "change_coordXY",
+      "message0": "x: %1, y: %2 위치로 이동하기",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "x",
+          "check": "Number",
+          "align": "RIGHT"
+        },
+        {
+          "type": "input_value",
+          "name": "y",
+          "check": "Number",
+          "align": "RIGHT"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "style": "move_blocks",
+      "tooltip": "입력한 x,y좌표로 요소를 이동시킵니다.",
       "helpUrl": ""
     },
 
@@ -213,7 +333,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#FFCC66",
+      "style": "move_blocks",
       "tooltip": "시계방향으로 angle만큼 회전합니다.",
       "helpUrl": ""
     },
@@ -236,8 +356,19 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#FFCC66",
+      "style": "move_blocks",
       "tooltip": "시계방향으로 angle만큼 회전합니다.",
+      "helpUrl": ""
+    },
+
+    // 마우스 위치로 이동시키는 블록
+    {
+      "type": "move_to_mouse",
+      "message0": "마우스 위치로 이동하기",
+      "previousStatement": null,
+      "nextStatement": null,
+      "style": "move_blocks",
+      "tooltip": "현재 오브젝트를 마우스 위치로 이동합니다.",
       "helpUrl": ""
     },
 
@@ -248,7 +379,7 @@ const defineMyBlocks = ()=>{
       "message0": "모양 보이기",
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "오브젝트를 화면에 보이게 합니다.",
       "helpUrl": ""
     },
@@ -259,7 +390,7 @@ const defineMyBlocks = ()=>{
       "message0": "모양 숨기기",
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "오브젝트를 화면에서 숨깁니다.",
       "helpUrl": ""
     },
@@ -277,7 +408,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "특정 요소 위에 말풍선을 표시합니다.",
       "helpUrl": ""
     },
@@ -306,7 +437,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "색상, 밝기, 투명도를 변경합니다.",
       "helpUrl": ""
     },
@@ -325,7 +456,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "요소의 크기를 지정한 값만큼 증가/감소합니다.",
       "helpUrl": ""
     },
@@ -344,7 +475,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "요소의 크기를 지정한 값으로 변경합니다.",
       "helpUrl": ""
     },
@@ -365,7 +496,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "오브젝트를 좌우 또는 상하로 반전시킵니다.",
       "helpUrl": ""
     },
@@ -384,7 +515,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#99CC66",
+      "style": "looks_blocks",
       "tooltip": "지정된 이미지 URL로 이미지를 변경합니다.",
       "helpUrl": ""
     },
@@ -403,7 +534,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#6699FF",
+      "style": "sound_blocks",
       "tooltip": "주어진 소리 파일을 재생합니다.",
       "helpUrl": ""
     },
@@ -427,7 +558,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#6699FF",
+      "style": "sound_blocks",
       "tooltip": "주어진 소리 파일을 특정 시간 동안 재생합니다.",
       "helpUrl": ""
     },
@@ -457,7 +588,7 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#6699FF",
+      "style": "sound_blocks",
       "tooltip": "주어진 소리 파일을 특정 구간에서만 재생합니다.",
       "helpUrl": ""
     },
@@ -479,10 +610,30 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#6699FF",
+      "style": "sound_blocks",
       "tooltip": "소리를 멈춥니다. 전체를 멈추거나 하나만 멈출 수 있습니다.",
       "helpUrl": ""
     },
+
+    // 소리 크기 정하기 블록
+    {
+      "type": "set_sound_volume",
+      "message0": "소리 크기를 %1 %% 로 정하기",
+      "args0": [
+        {
+          "type": "field_number",
+          "name": "volume",
+          "value": 100,
+          "min": 0,
+          "max": 100
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "style": "sound_blocks",
+      "tooltip": "모든 소리의 크기를 입력한 퍼센트(%)로 설정합니다.",
+      "helpUrl": ""
+    },    
     
     // 소리 빠르기 배수 설정 블록
     {
@@ -498,10 +649,28 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#6699FF",
+      "style": "sound_blocks",
       "tooltip": "주어진 소리 파일의 재생 속도를 몇 배로 설정합니다.",
       "helpUrl": ""
     },
+
+    // 판단 관련 블록들
+    // 접촉 여부 판단 블록
+    {
+      "type": "is_touching",
+      "message0": "%1 에 닿았는가?",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "TARGET",
+          "options": getTouchDropdown
+        }
+      ],
+      "output": "Boolean",
+      "style": "logic_blocks",
+      "tooltip": "선택된 오브젝트가 대상에 닿았는지 확인합니다.",
+      "helpUrl": ""
+    },    
 
     // 계산 관련 블록들
     // 초시계 생성 블록
@@ -520,18 +689,20 @@ const defineMyBlocks = ()=>{
       ],
       "previousStatement": null,
       "nextStatement": null,
-      "colour": "#668493",
+      "style": "math_blocks",
       "tooltip": "초시계를 시작하거나 정지합니다.",
       "helpUrl": ""
     },
+
+    // 초시게 값 반환 블록
     {
       "type": "get_timer_value",
       "message0": "초시계 값",
       "output": "Number",
-      "colour": "#668493",
+      "style": "math_blocks",
       "tooltip": "현재 초시계 값을 반환합니다.",
       "helpUrl": ""
-    }
+    },
   ]);
 };
 
