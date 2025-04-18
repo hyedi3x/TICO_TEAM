0aa65305c7ed080b9968c4c127cd5cff62be2eeb
meldy22 <anzngksduswn@naver.com>
2025-04-18 19:02:04 +0900

    [Comment] WebConfig.java
    - 주석 추가

--- src/main/java/com/boot/tico/project/config/WebConfig.java
+++ src/main/java/com/boot/tico/project/config/WebConfig.java
@@ -12,7 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // URL 요청: /uploads/~~~  → 실제 폴더: D:/Git/TICO_TEAM/uploads/로 연동
 		System.out.println("🧭 현재 작업 경로: " + System.getProperty("user.dir"));
+		
+		// 정적 자원 요청 URL → 실제 경로 매핑
         registry.addResourceHandler("/uploads/**") // 와일드카드 ** : **는 0개 이상의 디렉토리와 파일을 매칭
                 .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+        		// "file:" 접두사는 로컬 파일 시스템에서 파일을 찾겠다는 의미
     }  
 }
