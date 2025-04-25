2ec5bfcee5aa8dab8ae52040155a3f29c3d43989
wpzmfhtm <wpzmfhtm@naver.com>
2025-04-25 09:33:37 +0900

    [modify] User.java(멤버변수 중복된 변수 제거)

--- src/main/java/com/boot/tico/login/entity/User.java
+++ src/main/java/com/boot/tico/login/entity/User.java
@@ -55,14 +55,6 @@ public class User {
 
     @Column(name = "provider_id", length = 255)
     private String providerId;
-    
-    // refresh token 컬럼
-    @Column(name = "refresh_token", length = 255)
-    private String refreshToken;
-
-    // 만료일시 컬럼
-    @Column(name = "refresh_token_expiry")
-    private LocalDateTime refreshTokenExpiry;
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
