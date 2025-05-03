c5bb9dea1fe974dede25d6808946683729f86b6e
meldy22 <anzngksduswn@naver.com>
2025-05-03 22:58:21 +0900

    [feat] ProjectReportController & Service & DTO
    - ProjectReportController
    - POST /api/report : 신고 접수 저장 기능 구현
    - GET /api/report/list : 관리자용 신고 목록 조회 (검색 + 페이징)
    - 신고 시 ProjectReportDTO 사용, createdAt 기준 정렬 적용
    
    - ProjectReportSerivce
    - 신고 저장 시 reportId 수동 생성 및 트랜잭션 적용
    - 관리자용 신고 목록 전체/검색 조회 메서드 구현
    - Pageable 기반 페이징 처리
    
    - ProjectReportDTO.java
    - project_report_tb 테이블과 매핑
    - user 연관 관계 설정 (LAZY)
    - 신고 사유, 작품 ID, 생성일 포함

--- src/main/java/com/boot/tico/project/dto/ProjectReportDTO.java
+++ src/main/java/com/boot/tico/project/dto/ProjectReportDTO.java
@@ -4,9 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
+
+import com.boot.tico.login.entity.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,16 +30,21 @@ import lombok.ToString;
 @Entity
 @Table(name = "project_report_tb")
 public class ProjectReportDTO {
-
+	
+	@ManyToOne(fetch = FetchType.LAZY)	// ManyToOne : 다대일 관계를 설정. fetch = FetchType.LAZY : 연관된 엔티티(User)를 지연 로딩. 즉, report.getUser()를 호출하기 전까지는 DB에서 User 정보를 가져오지 않음 → 성능 최적화에 좋음
+	@JoinColumn(name = "user_uuid", insertable = false, updatable = false)	// user_uuid 필드가 이미 DTO에 존재하기 때문에, 연관관계용 필드는 읽기 전용으로 설정
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})	// 
+	private User user;
+	
+	@Column(name = "user_uuid")
+	private String userUuid;
+	
 	@Id
 	@Column(name = "report_id")
 	private int reportId;
 	
 	@Column(name = "project_id")
 	private int projectId;
-	
-	@Column(name = "user_uuid")
-	private String userUuid;
 	
 	@Column(name = "reason")
 	private String reason;
