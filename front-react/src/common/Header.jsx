1bfa4a3f3de8d82cec5c9860993bc7cab89db4d9
wpzmfhtm <wpzmfhtm@naver.com>
2025-05-03 18:47:42 +0900

    [modify] CSS 수정
    - ShareGallery.css - 미디어쿼리 설정
    - MyProjects.js - style로 공개/비공개 여부(Badge 컴포넌트) 글씨 및 전체 크기 증가
    - Header.jsx - Mypage 넘어가는 부분에 닉네임 추가

--- front-react/src/common/Header.jsx
+++ front-react/src/common/Header.jsx
@@ -122,7 +122,7 @@ function Header() {
                       </Dropdown.Toggle>
                       <Dropdown.Menu>
                         <Dropdown.Item onClick={() => navigate('/MypageMain')}>
-                          Mypage
+                          {user?.nickname}님의 페이지
                         </Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
