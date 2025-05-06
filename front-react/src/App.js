072d3d053b25678f03015f0be8f8249b198e7310
wpzmfhtm <wpzmfhtm@naver.com>
2025-05-07 00:01:23 +0900

    [modify] url 추가

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Header from './common/Header';
 import Main from './common/Main';
 import Footer from './common/Footer';
@@ -38,6 +39,9 @@ import Notice from './pages/notice/Notice';
 import NoticeDetail from './pages/notice/NoticeDetail';
 import NoticePost from './pages/notice/NoticePost';
 import NoticeAdmin from './pages/notice/NoticeAdmin';
+import StudyCreate from './blockly/study/StudyCreate';
+import StudyShareGallery from './pages/share/StudyShareGallery';
+import StudyDetail2 from './pages/share/StudyDetail2';
 
 function App() {
   return (
@@ -81,6 +85,9 @@ function App() {
           <Route path="/noticeDetail/:noticeId" element={<NoticeDetail/>}/>
           <Route path="/noticePost/" element={<NoticePost/>}/>
           <Route path="/noticePut/:noticeId" element={<NoticePost/>}/>
+          <Route path="/study" element={<StudyCreate />} />
+          <Route path="/shareStudy" element={<StudyShareGallery />} />
+          <Route path="/study/detail/:studyId" element={<StudyDetail2 />} />
         </Routes>
         <Footer />
       </BrowserRouter>
