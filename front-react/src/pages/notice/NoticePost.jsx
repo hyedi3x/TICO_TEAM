7bbc8f44d374abd1628d67badc6733926af42746
cruelSummer12 <rlatkdgus100@naver.com>
2025-05-07 19:44:18 +0900

    [modify] NoticePost.jsx - 이동 경로 수정

--- front-react/src/pages/notice/NoticePost.jsx
+++ front-react/src/pages/notice/NoticePost.jsx
@@ -55,7 +55,7 @@ function NoticePost({ noticeId, onClose }) {
       axiosInstance.put(`/api/noticePut/${noticeId}`, dto)
         .then(() => {
           alert("공지 수정 완료!");
-          onClose(); // 모달 닫기
+          navigate("/noticeList");
         })
         .catch(err => {
           console.error("수정 실패:", err);
